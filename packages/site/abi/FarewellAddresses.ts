d2f456a0e922d0eda8a7e245b981b2efda71828e
Pedro Alves <pdroalves@gmail.com>
2025-09-14 06:27:18 -0300

    Update contract

--- packages/site/abi/FarewellAddresses.ts
+++ packages/site/abi/FarewellAddresses.ts
@@ -4,6 +4,6 @@
   Command: 'npm run genabi'
 */
 export const FarewellAddresses = { 
-  "11155111": { address: "0x6459087492b6F8aa6314559DdB0F90Fc2B54FE22", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0xF544D730eb2a4c8E56a0A5b785a683e9DA30A2db", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0xf3345D0529982eB9D9F41D71Fb725E8C4031b1dd", chainId: 31337, chainName: "hardhat" },
 };
